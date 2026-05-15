20df95cdb2aa310845dc4cf675e00a23f6704682
elysia <3302843873@qq.com>
2026-05-15 20:40:02 +0800

    implemented run_basic_kernel

--- xtask/src/run_basic_kernel.rs
+++ xtask/src/run_basic_kernel.rs
@@ -1,5 +1,20 @@
 use anyhow::Result;
 
+use crate::{
+    build_image::{bootloader_image, build_image},
+    run_vm::{VMResult, run_vm},
+};
+
 pub fn run_basic_kernel() -> Result<()> {
-    todo!()
+    build_image()?;
+    let result = run_vm(bootloader_image()?);
+    display_vm_result(result);
+    Ok(())
+}
+
+fn display_vm_result(result: VMResult) {
+    match result {
+        Ok(exit_status) => println!("Qemu exited with status {exit_status}"),
+        Err(error) => println!("{error}"),
+    }
 }
