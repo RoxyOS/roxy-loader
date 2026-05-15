ea01d7dccc191672c0dca0c033e2f2bfaf233fc4
elysia <3302843873@qq.com>
2026-05-15 19:57:39 +0800

    xtask: cargo_target_dir

--- xtask/src/utils.rs
+++ xtask/src/utils.rs
@@ -1,7 +1,20 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use cargo_metadata::MetadataCommand;
+
 #[macro_export]
 macro_rules! run_command {
     ($cmd:literal) => {
         let sh = $crate::private::xshell::Shell::new()?;
         $crate::private::xshell::cmd!(sh, $cmd).run()?;
     };
+}
+
+pub fn cargo_target_dir() -> Result<PathBuf> {
+    Ok(cargo_metadata()?.target_directory.into())
+}
+
+fn cargo_metadata() -> Result<cargo_metadata::Metadata> {
+    Ok(cargo_metadata::MetadataCommand::new().exec()?)
 }
