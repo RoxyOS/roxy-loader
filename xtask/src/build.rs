b10183a058e2988070d12c0b59201e4d11c9ec2c
elysia <3302843873@qq.com>
2026-05-15 19:57:50 +0800

    xtask: bootloader_efi_path

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -1,9 +1,18 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use workspace_root::get_workspace_root;
 
-use crate::run_command;
+use crate::{run_command, utils::cargo_target_dir};
 
 pub fn build() -> Result<()> {
     run_command!("cargo build --target x86_64-unknown-uefi");
     Ok(())
+}
+
+pub fn bootloader_efi_path() -> Result<PathBuf> {
+    Ok(cargo_target_dir()?
+        .join("x86_64-unknown-uefi")
+        .join("debug")
+        .join("roxy-loader.efi"))
 }
