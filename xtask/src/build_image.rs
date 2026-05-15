e4f94a3967d1084008d85c81df3ea14b5104cbdd
elysia <3302843873@qq.com>
2026-05-15 20:02:23 +0800

    implemtned build_image

--- xtask/src/build_image.rs
+++ xtask/src/build_image.rs
@@ -1,5 +1,13 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use image_builder::build_bootloader_image;
+use workspace_root::get_workspace_root;
+
+use crate::{build::bootloader_efi_file, utils::cargo_target_dir};
+
+const IMAGE_PATH: &str = "image.img";
 
 pub fn build_image() -> Result<()> {
-    todo!()
+    let bootloader_image = cargo_target_dir()?.join(IMAGE_PATH);
+    build_bootloader_image(bootloader_efi_file()?, bootloader_image)?;
+    Ok(())
 }
