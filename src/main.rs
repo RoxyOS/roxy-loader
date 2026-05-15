63316859683c1a5448c78bd063ae84fdbce00833
elysia <3302843873@qq.com>
2026-05-15 11:05:21 +0800

    sleeps for 10 secs after pritning

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
 
-use uefi::{Status, entry, println};
+use core::time::Duration;
+
+use uefi::{Status, boot, entry, println};
 
 #[entry]
 fn main() -> Status {
     println!("hello world!!");
+    boot::stall(Duration::from_secs(10));
 
     Status::SUCCESS
 }
