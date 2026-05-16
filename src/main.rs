34d906c285912c840925761025978127f1cba395
elysia <3302843873@qq.com>
2026-05-16 13:02:31 +0800

    finished basic bootloader

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@
 
 extern crate alloc;
 
-use core::{ptr::copy_nonoverlapping, time::Duration};
+use core::{mem::transmute, ptr::copy_nonoverlapping, time::Duration};
 
-use elfloader::{ElfLoader, ElfLoaderErr};
+use alloc::ffi::c_str;
+use anyhow::{Context, Result};
+use core::result::Result::Ok;
+use elfloader::{ElfBinary, ElfLoader, ElfLoaderErr};
 use uefi::{
     Status,
     boot::{self, AllocateType, MemoryType, allocate_pages, exit_boot_services},
@@ -13,13 +16,40 @@ use uefi::{
     proto::media::fs,
 };
 
+use crate::{elf_loader::RoxyElfLoader, utils::read_file};
+use uefi::cstr16;
+
 mod elf_loader;
 mod utils;
 
 #[entry]
 fn main() -> Status {
-    println!("hello world!!");
-    boot::stall(Duration::from_secs(10));
+    match run() {
+        Ok(()) => Status::SUCCESS,
+        Err(error) => {
+            println!("{error}");
+            loop {}
+        }
+    }
+}
 
-    Status::SUCCESS
+fn run() -> Result<()> {
+    let kernel_file = read_file(cstr16!("/kernel")).context("Failed to read kernel file")?;
+    let kernel_elf = ElfBinary::new(&kernel_file)
+        .ok()
+        .context("Failed to parse kernel elf")?;
+
+    kernel_elf
+        .load(&mut RoxyElfLoader)
+        .expect("Failed to load kernel elf");
+
+    unsafe {
+        exit_boot_services(None);
+
+        let kernel_entry: extern "C" fn() = transmute(kernel_elf.entry_point());
+
+        kernel_entry();
+    }
+
+    Ok(())
 }
