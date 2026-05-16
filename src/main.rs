73f04db2b5448c4afe446f8ce32c54f26ff7d0e0
elysia <3302843873@qq.com>
2026-05-16 11:35:22 +0800

    implemented elfloader

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::time::Duration;
+use core::{ptr::copy_nonoverlapping, time::Duration};
 
-use uefi::{Status, boot, entry, println};
+use elfloader::{ElfLoader, ElfLoaderErr};
+use uefi::{
+    Status,
+    boot::{self, AllocateType, MemoryType, allocate_pages, exit_boot_services},
+    entry, println,
+    proto::media::fs,
+};
 
 #[entry]
 fn main() -> Status {
@@ -11,4 +17,41 @@ fn main() -> Status {
     boot::stall(Duration::from_secs(10));
 
     Status::SUCCESS
+}
+
+struct RoxyLoader;
+
+impl ElfLoader for RoxyLoader {
+    fn allocate(
+        &mut self,
+        load_headers: elfloader::LoadableHeaders,
+    ) -> Result<(), elfloader::ElfLoaderErr> {
+        for header in load_headers {
+            let addr = header.physical_addr();
+            let size = header.mem_size();
+            let pages = size.div_ceil(4096) as usize;
+
+            allocate_pages(AllocateType::Address(addr), MemoryType::LOADER_DATA, pages)
+                .map_err(|_| ElfLoaderErr::OutOfMemory)?;
+        }
+
+        Ok(())
+    }
+
+    fn load(
+        &mut self,
+        _flags: elfloader::Flags,
+        base: elfloader::VAddr,
+        region: &[u8],
+    ) -> Result<(), ElfLoaderErr> {
+        unsafe {
+            copy_nonoverlapping(region.as_ptr(), base as *mut u8, region.len());
+        }
+
+        Ok(())
+    }
+
+    fn relocate(&mut self, _entry: elfloader::RelocationEntry) -> Result<(), ElfLoaderErr> {
+        panic!("Relocate is not supported.")
+    }
 }
