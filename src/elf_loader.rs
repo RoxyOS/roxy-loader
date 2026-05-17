7c78afaa6fb3eafffcd976bf925473b582ed5f5e
elysia <3302843873@qq.com>
2026-05-17 23:34:21 +0800

    fixed elfloader bug and zeros allocated page

--- src/elf_loader.rs
+++ src/elf_loader.rs
@@ -1,10 +1,7 @@
-use core::ptr::copy_nonoverlapping;
+use core::ptr::{copy_nonoverlapping, write_bytes};
 
 use elfloader::{ElfLoader, ElfLoaderErr};
-use uefi::{
-    boot::{AllocateType, MemoryType, allocate_pages},
-    println,
-};
+use uefi::boot::{AllocateType, MemoryType, allocate_pages};
 
 pub struct RoxyElfLoader;
 
@@ -14,12 +11,28 @@ impl ElfLoader for RoxyElfLoader {
         load_headers: elfloader::LoadableHeaders,
     ) -> Result<(), elfloader::ElfLoaderErr> {
         for header in load_headers {
+            const PAGE_SIZE: u64 = 4096;
+
             let addr = header.virtual_addr();
             let size = header.mem_size();
-            let pages = size.div_ceil(4096) as usize;
 
-            allocate_pages(AllocateType::Address(addr), MemoryType::LOADER_DATA, pages)
-                .expect("Failed to allocate page for kernel elf");
+            let page_base = addr & !(PAGE_SIZE - 1);
+            let page_offset = addr - page_base;
+            let alloc_size = page_offset + size;
+            let pages = alloc_size.div_ceil(PAGE_SIZE) as usize;
+
+            let allocated_ptr = allocate_pages(
+                AllocateType::Address(page_base),
+                MemoryType::LOADER_DATA,
+                pages,
+            )
+            .expect("Failed to allocate page for kernel elf")
+            .as_ptr();
+
+            // Zero allocated pages
+            unsafe {
+                write_bytes(allocated_ptr, 0, pages * PAGE_SIZE as usize);
+            }
         }
 
         Ok(())
