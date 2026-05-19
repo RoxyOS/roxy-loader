606578c616f18e8cdf1cd1db0be64ff64e6ccebb
elysia <3302843873@qq.com>
2026-05-19 18:19:41 +0800

    added publish scrtip

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cargo publish -p roxy-loader-api
+echo "waiting 10 seconds..."
+sleep 10
+cargo publish -p roxy-loader
+echo "waiting 10 seconds..."
+sleep 10
+cargo publish -p roxy-loader-utils
