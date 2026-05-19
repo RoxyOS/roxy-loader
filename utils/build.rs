c3ee8466107664dc0b5385020b0c43e47526f00b
elysia <3302843873@qq.com>
2026-05-19 23:01:46 +0800

    added debug msg and bumped version to 0.1.1-alpha

--- utils/build.rs
+++ utils/build.rs
@@ -1,6 +1,21 @@
 use cargo_emit::rustc_env;
 
 fn main() {
+    for (key, value) in std::env::vars() {
+        if key.starts_with("CARGO_BIN_FILE_") {
+            eprintln!("{key}={value}");
+        }
+    }
+
+    eprintln!(
+        "HOST={}",
+        std::env::var("HOST").unwrap_or_else(|_| "<unset>".to_string())
+    );
+    eprintln!(
+        "TARGET={}",
+        std::env::var("TARGET").unwrap_or_else(|_| "<unset>".to_string())
+    );
+
     let Some(roxyloader_artifact) = cargo_bin_file::bin_path("roxy-loader") else {
         panic!("Roxyloader artifact not found")
     };
