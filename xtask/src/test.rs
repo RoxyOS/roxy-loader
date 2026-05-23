3b402d4129296d93edc1d195f955b2cb5fe01239
elysia <3302843873@qq.com>
2026-05-23 17:35:03 +0800

    enables local-dev when runnign cargo xtest

--- xtask/src/test.rs
+++ xtask/src/test.rs
@@ -4,7 +4,8 @@ use crate::{run_command, test_kernel::run_test_kernel};
 
 pub fn test() -> Result<()> {
     run_command!("cargo test -p roxy-loader --lib");
-    run_command!("cargo test -p roxy-loader-api -p roxy-loader-utils -p xtask");
+    run_command!("cargo test -p roxy-loader-api -p xtask");
+    run_command!("cargo test -p roxy-loader-utils --features local-dev");
     run_command!("cargo test --doc -p roxy-loader-api -p roxy-loader-utils");
     run_test_kernel()?;
     Ok(())
