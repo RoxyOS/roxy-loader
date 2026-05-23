27b3d61176ae62881a3ac6c470b097c51d00b677
elysia <3302843873@qq.com>
2026-05-23 17:57:55 +0800

    deleted unnessery docs

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -5,9 +5,6 @@
 //! If you want to use `roxy-loader`, see the [Github README](https://github.com/RoxyOS/roxy-loader).
 //! These docs are not intended to be guides.
 
-/// Boot-time information provided to the kernel.
 pub mod bootinfo;
-/// Framebuffer types provided to the kernel.
 pub mod framebuffer;
-/// Macros for defining the kernel entry point.
 pub mod kernel_entry;
